ce29834bde1dd3bb6efe0c8a9a5fd9aa3d1c78a5
Carlos <carlos.rodriguez124@upr.edu>
2020-02-17 14:11:10 -0400

    Buttons added

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -14,10 +14,19 @@ import java.awt.Button;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextPane;
+import javax.swing.JFormattedTextField;
+import java.awt.TextField;
+import javax.swing.JEditorPane;
+import javax.swing.DropMode;
 
 public class UserInterface extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtx;
+	private JTextField txtY;
 
 	/**
 	 * Launch the application.
@@ -46,14 +55,6 @@ public class UserInterface extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton x = new JButton("x");
-		x.setBounds(25, 75, 50, 50);
-		contentPane.add(x);
-		
-		JButton y = new JButton("y");
-		y.setBounds(85, 75, 50, 50);
-		contentPane.add(y);
-		
 //		JLabel backgroundCC;
 //		Image img = new ImageIcon(this.getClass().getResource("/Background-cc.png")).getImage();
 //		backgroundCC = new JLabel("", JLabel.CENTER);
@@ -61,12 +62,38 @@ public class UserInterface extends JFrame {
 //		backgroundCC.setBounds(0, 0, 800, 600);
 //		getContentPane().add(backgroundCC);
 		
+		
+		txtx = new JTextField();
+		txtx.setText("X");
+		txtx.setBounds(0, 10, 51, 32);
+		contentPane.add(txtx);
+		txtx.setColumns(10);
+		
+		txtY = new JTextField();
+		txtY.setText("Y");
+		txtY.setBounds(109, 10, 51, 32);
+		contentPane.add(txtY);
+		txtY.setColumns(10);
+		
+		JRadioButton rdbtnPolarPlane = new JRadioButton("Polar Plane");
+		rdbtnPolarPlane.setBounds(0, 153, 201, 35);
+		contentPane.add(rdbtnPolarPlane);
+		
+		JRadioButton rdbtnCartesianPlane = new JRadioButton("Cartesian Plane");
+		rdbtnCartesianPlane.setBounds(0, 201, 201, 35);
+		contentPane.add(rdbtnCartesianPlane);
+		
 		JLabel backgroundCP;
 		Image img2 = new ImageIcon(this.getClass().getResource("/Background-cp.png")).getImage();
+		
+		JButton btnGraphic = new JButton("Graphic");
+		btnGraphic.setBounds(21, 58, 141, 35);
+		contentPane.add(btnGraphic);
 		backgroundCP = new JLabel("", JLabel.CENTER);
 		backgroundCP.setIcon(new ImageIcon(img2));
 		backgroundCP.setBounds(0, 0, 800, 600);
 		getContentPane().add(backgroundCP);
 		
+	
 	}
 }
