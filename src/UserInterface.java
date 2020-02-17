1c498197fdd14ba2aad3ce36795fb7bc567b8d83
Carlos <carlos.rodriguez124@upr.edu>
2020-02-17 17:41:22 -0400

    Draw Circles and Lines

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -9,6 +10,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
 import java.awt.Panel;
+import java.awt.Point;
 import java.awt.Label;
 import java.awt.Button;
 import java.awt.Color;
@@ -19,8 +21,13 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextPane;
 import javax.swing.JFormattedTextField;
 import java.awt.TextField;
+import java.util.ArrayList;
+import java.util.Random;
+
 import javax.swing.JEditorPane;
 import javax.swing.DropMode;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UserInterface extends JFrame {
 
@@ -29,6 +36,9 @@ public class UserInterface extends JFrame {
 	private JTextField yButton;
 	private JTextField rButton;
 	private JTextField angleButton;
+	ArrayList<Integer> x= new ArrayList<Integer>();
+	ArrayList<Integer> y= new ArrayList<Integer>();
+	
 
 	/**
 	 * Launch the application.
@@ -45,11 +55,35 @@ public class UserInterface extends JFrame {
 			}
 		});
 	}
+	
+	public void paint(Graphics g){
+	         // Circular Surface 
+		super.paint(g);
+		for(int i=0;i<x.size();i++) {
+			this.drawCircleByCenter(g, x.get(i), y.get(i));
+		}
+		for(int i=x.size()-1;i>0;i--) {
+			g.drawLine(x.get(i),y.get(i), x.get(i-1), y.get(i-1));
+		}
+		
+		
+			
+	      
+	     }
+
+	     void drawCircleByCenter(Graphics g, int x, int y ){
+	    	 g.drawOval(x, y, 5, 5);
+	         g.fillOval(x, y, 5, 5);
+	        
+	     }
+	     
 
 	/**
 	 * Create the frame.
 	 */
 	public UserInterface() {
+		x.add(523);
+		y.add(342);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
@@ -58,13 +92,13 @@ public class UserInterface extends JFrame {
 		contentPane.setLayout(null);	
 		
 		xButton = new JTextField();
-		xButton.setText("  x");
+		xButton.setText("X");
 		xButton.setBounds(30, 90, 35, 35);
 		contentPane.add(xButton);
 		xButton.setColumns(10);
 		
 		yButton = new JTextField();
-		yButton.setText("  y");
+		yButton.setText("Y");
 		yButton.setColumns(10);
 		yButton.setBounds(75, 90, 35, 35);
 		contentPane.add(yButton);
@@ -82,6 +116,14 @@ public class UserInterface extends JFrame {
 		contentPane.add(angleButton);
 		
 		JButton btnGraph = new JButton("Graph");
+		btnGraph.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				  x.add(Integer.parseInt(xButton.getText()));
+				  y.add(Integer.parseInt(yButton.getText()));
+				  
+				    repaint();
+			}
+		});
 		btnGraph.setBounds(33, 175, 75, 35);
 		contentPane.add(btnGraph);
 		
@@ -110,4 +152,5 @@ public class UserInterface extends JFrame {
 		
 	
 	}
+	
 }
